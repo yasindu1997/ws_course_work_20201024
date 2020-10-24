6f685c6017fa4a5bbaa163dcf316be3f8f71aebe
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-24 15:51:31 +0530

    Remove comments

--- src/main/java/lk/ijse/webservice/resource_access/RestServer.java
+++ src/main/java/lk/ijse/webservice/resource_access/RestServer.java
@@ -25,31 +25,5 @@ public class RestServer {
         server.setHandler(context);
         server.start();
         server.join();
-//        server = new Server();
-//        ServerConnector connector = new ServerConnector(server);
-//        connector.setPort(8080);
-//        server.setConnectors(new Connector[]{connector});
-//
-//        ServletContextHandler context=new ServletContextHandler(ServletContextHandler.SESSIONS);
-//
-//        // Specify the Session ID Manager
-//        SessionIdManager idmanager = new DefaultSessionIdManager(server);
-//        server.setSessionIdManager(idmanager);
-//
-//        SessionHandler sessionHandler= new SessionHandler();
-//        sessionHandler.setSessionIdManager(idmanager);
-//
-//        ServletHandler servletHandler = new ServletHandler();
-//        context.setHandler(servletHandler);
-//
-//
-//        servletHandler.addServletWithMapping(ResourceAccessRest.class, "/hello");
-//
-//
-//        context.setHandler(sessionHandler);
-//
-//
-//        server.setHandler(context);
-//        server.start();
     }
 }
